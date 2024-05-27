33b8f4fb029e7081d42a7aae337b718c947d6d22
YaoSong808 <jp2021212814@qmul.ac.uk>
2024-05-27 21:48:38 +0800

    delete useless materials

--- KidsBank6(1)/KidsBank6/docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.kidsbank"},{"l":"com.kidsbank.controller"},{"l":"com.kidsbank.entity"},{"l":"com.kidsbank.util"}];updateSearchResults();
